33b86d426d79d3945df006d51132093de4f0902c
Vira Lytvyn <baterfluy@gmail.com>
2025-08-12 17:04:58 +0300

    remove redirect to mimic old app functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,7 @@ import { NotFoundComponent } from '@shared/not-found/not-found.component';
 import { TodoComponent } from '@features/todo/todo/todo.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'todo', pathMatch: 'full' },
-  { path: 'todo', component: TodoComponent },
+  { path: '', component: TodoComponent },
   // ...other app routes
   { path: '**', component: NotFoundComponent },
 ];

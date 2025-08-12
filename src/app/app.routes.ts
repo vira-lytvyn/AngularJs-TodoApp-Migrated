100e4492fc0c1fd527541c51389a3722c4e75704
Vira Lytvyn <baterfluy@gmail.com>
2025-08-12 16:32:28 +0300

    Add todo as a default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,8 @@ import { NotFoundComponent } from '@shared/not-found/not-found.component';
 import { TodoComponent } from '@features/todo/todo/todo.component';
 
 export const routes: Routes = [
-  { path: '', component: TodoComponent },
+  { path: '', redirectTo: 'todo', pathMatch: 'full' },
+  { path: 'todo', component: TodoComponent },
   // ...other app routes
   { path: '**', component: NotFoundComponent },
 ];
